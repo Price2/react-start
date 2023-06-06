a198101298411aa018d765c06dbe3ace484aab4f
ITWORX\Daniel.Habib <daniel.habib@itworx.com>
2023-06-06 17:03:20 +0300

    submitting task

---
+++ react/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import image from "./react.gif"
+const App = () =>{
+    return (
+        <>
+        <h1>
+            Hello world! I am using React
+        </h1>
+        <img src={image}/>
+        </>
+    )
+}
+
+export default App
---
+++ react/webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: '/src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  target: 'web',
+  devServer: {
+    port: '5000',
+    static: {
+      directory: path.join(__dirname, 'public')
+},
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // .js and .jsx files
+        exclude: /node_modules/, // excluding the node_modules folder
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/, // styles files
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              context: 'project',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ]
+};
